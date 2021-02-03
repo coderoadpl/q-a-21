3f0d8aa9996137ac888b15d4ca3733fef4e6aea5
Mateusz Choma <chomamateusz@gmail.com>
2021-02-03 20:41:18 +0100

    DB connected and messages are read

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const mysql      = require('mysql')
 require('dotenv').config()
 
 const app = express()
 const port = 3333
+
+const connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE,
+})
+ 
+connection.connect()
+
+connection.query('SELECT * FROM messages', (error, results, fields) => {
+  if (error) throw error
+  console.log(results)
+})
 
 app.use('/', express.static('public'))
 
