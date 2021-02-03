c71fb35ba92291c82463289b586039e30320a18a
Mateusz Choma <chomamateusz@gmail.com>
2021-02-03 20:15:23 +0100

    Cleared nodemon and sending emails

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 const express = require('express')
-const nodemailer = require('nodemailer')
 require('dotenv').config()
 
 const app = express()
 const port = 3333
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // upgrade later with STARTTLS
-  auth: {
-    user: "andisyy@gmail.com",
-    pass: process.env.SMTP_PASSWORD
-  }
-})
-
 app.use('/', express.static('public'))
-
-const SUBJECT = 'Mail from CodeRoad form!'
 
 app.get('/message', (req, res) => {
   const { message, email } = req.query
@@ -25,23 +12,10 @@ app.get('/message', (req, res) => {
   if(!message) return res.status(400).send()
   if(!email) return res.status(400).send()
 
-  console.log('Starting to send message: ', message, email)
-
-  const response = transporter.sendMail(
-    {
-      from: "andisyy@gmail.com",
-      to: email,
-      subject: SUBJECT,
-      text: message,
-    },
-    (err, info) => {
-      console.log('Message sent callback: ', err, info)
-      if(err) return res.status(500).send()
-      res.status(200).send(info)
-    }
-  )
-
-  console.log(response)
+  res.json({
+    message,
+    email
+  })
 
 })
 
